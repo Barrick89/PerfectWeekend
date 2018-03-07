699f89f35739d477535d7d5da7c4ea9c82e69040
marku <markus.hauschild.negativ@googlemail.com>
2018-03-07 20:56:27 +0100

    Add table contract

---
+++ app/src/main/java/com/mahausch/perfectweekend/data/LocationContract.java
@@ -0,0 +1,29 @@
+package com.mahausch.perfectweekend.data;
+
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class LocationContract {
+
+    private LocationContract() {
+    }
+
+    public static final String CONTENT_AUTHORITY = "com.mahausch.perfectweekend";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_LOCATIONS = "locations";
+
+    public static abstract class LocationEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATIONS).build();
+
+        public static final String TABLE_NAME = "locations";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_LOCATION_NAME = "name";
+        public static final String COLUMN_LOCATION_IMAGE = "image";
+        public static final String COLUMN_LOCATION_DESCRIPTION = "description";
+        public static final String COLUMN_LOCATION_POSITION = "position";
+    }
+}
