3bce7a79abac4fcc9b6243f4dfd2ebfdfb629fb4
marku <markus.hauschild.negativ@googlemail.com>
2018-03-07 21:28:56 +0100

    Add insert methods

--- app/src/main/java/com/mahausch/perfectweekend/data/LocationProvider.java
+++ app/src/main/java/com/mahausch/perfectweekend/data/LocationProvider.java
@@ -3,11 +3,14 @@ package com.mahausch.perfectweekend.data;
 
 import android.content.ContentProvider;
 import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.mahausch.perfectweekend.data.LocationContract.LocationEntry;
 
@@ -57,6 +60,52 @@ public class LocationProvider extends ContentProvider {
         cursor = db.query(table, projection, selection, selectionArgs, null, null, sortOrder);
         cursor.setNotificationUri(getContext().getContentResolver(), uri);
         return cursor;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case LOCATION:
+                return insertLocation(uri, contentValues);
+            default:
+                throw new IllegalArgumentException("Insertion is not supported for " + uri);
+        }
+    }
+
+    private Uri insertLocation(Uri uri, ContentValues values) {
+
+        String name = values.getAsString(LocationEntry.COLUMN_LOCATION_NAME);
+        if (name == null) {
+            throw new IllegalArgumentException("Location requires a name");
+        }
+
+        String image = values.getAsString(LocationEntry.COLUMN_LOCATION_IMAGE);
+        if (image == null) {
+            throw new IllegalArgumentException("Location requires valid image");
+        }
+
+        String description = values.getAsString(LocationEntry.COLUMN_LOCATION_DESCRIPTION);
+        if (description == null) {
+            description = "";
+        }
+
+        String position = values.getAsString(LocationEntry.COLUMN_LOCATION_POSITION);
+        if (position == null) {
+            throw new IllegalArgumentException("Location requires valid position");
+        }
+
+        SQLiteDatabase db = mLocationDbHelper.getWritableDatabase();
+
+        long id = db.insert(LocationEntry.TABLE_NAME, null, values);
+
+        if (id == -1) {
+            Log.e(LOG_TAG, "Failed to insert row for " + uri);
+            return null;
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return ContentUris.withAppendedId(uri, id);
     }
 
 }
