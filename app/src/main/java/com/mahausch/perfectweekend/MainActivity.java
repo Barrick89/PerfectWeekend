323d7e3e2a3dbc5e3dae6d3525854fc19bd4903e
marku <markus.hauschild.negativ@googlemail.com>
2018-03-11 11:24:38 +0100

    Add FAB and Recyclerview reference

--- app/src/main/java/com/mahausch/perfectweekend/MainActivity.java
+++ app/src/main/java/com/mahausch/perfectweekend/MainActivity.java
@@ -4,17 +4,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+
+    @BindView(R.id.recycler_view)
+    RecyclerView recycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.location_overview);
+        ButterKnife.bind(this);
 
         //Set OnClickListener for the FloatingActionButton to launch EditorActivity
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
