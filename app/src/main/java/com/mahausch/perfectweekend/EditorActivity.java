25e987b2c1ad919bbeb4560c1b082707f6a43324
marku <markus.hauschild.negativ@googlemail.com>
2018-03-12 22:43:31 +0100

    Override onCreate and add setContentView

--- app/src/main/java/com/mahausch/perfectweekend/EditorActivity.java
+++ app/src/main/java/com/mahausch/perfectweekend/EditorActivity.java
@@ -2,6 +2,8 @@ package com.mahausch.perfectweekend;
 
 
 import android.content.UriMatcher;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.mahausch.perfectweekend.data.LocationContract;
@@ -17,4 +19,11 @@ public class EditorActivity extends AppCompatActivity {
         sUriMatcher.addURI(LocationContract.CONTENT_AUTHORITY, LocationContract.PATH_LOCATIONS, LOCATION);
         sUriMatcher.addURI(LocationContract.CONTENT_AUTHORITY, LocationContract.PATH_LOCATIONS + "/#", LOCATION_ID);
     }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+
 }
