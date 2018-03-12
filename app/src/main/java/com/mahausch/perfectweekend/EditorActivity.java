069e9dd5cc8fed81e00b072f0a5579140ee5bb94
marku <markus.hauschild.negativ@googlemail.com>
2018-03-12 22:39:53 +0100

    Add UriMatcher

--- app/src/main/java/com/mahausch/perfectweekend/EditorActivity.java
+++ app/src/main/java/com/mahausch/perfectweekend/EditorActivity.java
@@ -1,5 +1,19 @@
 package com.mahausch.perfectweekend;
 
 
+import android.content.UriMatcher;
+
+import com.mahausch.perfectweekend.data.LocationContract;
+
 public class EditorActivity {
+
+    private static final int LOCATION = 100;
+    private static final int LOCATION_ID = 101;
+
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        sUriMatcher.addURI(LocationContract.CONTENT_AUTHORITY, LocationContract.PATH_LOCATIONS, LOCATION);
+        sUriMatcher.addURI(LocationContract.CONTENT_AUTHORITY, LocationContract.PATH_LOCATIONS + "/#", LOCATION_ID);
+    }
 }
