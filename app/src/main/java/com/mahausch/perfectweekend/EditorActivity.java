0faa44c3f119a05c231fcb66cb1b60d0d538a349
marku <markus.hauschild.negativ@googlemail.com>
2018-03-13 17:49:35 +0100

    Add logic to choose the activity title based on the current Uri

--- app/src/main/java/com/mahausch/perfectweekend/EditorActivity.java
+++ app/src/main/java/com/mahausch/perfectweekend/EditorActivity.java
@@ -1,15 +1,19 @@
 package com.mahausch.perfectweekend;
 
 
+import android.content.Intent;
 import android.content.UriMatcher;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.mahausch.perfectweekend.data.LocationContract;
+import com.mahausch.perfectweekend.data.LocationContract.LocationEntry;
 
 public class EditorActivity extends AppCompatActivity {
 
+    private static Uri currentUri;
     private static final int LOCATION = 100;
     private static final int LOCATION_ID = 101;
 
@@ -22,6 +26,18 @@ public class EditorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        Intent intent = getIntent();
+        if (intent.getExtras() != null) {
+            currentUri = intent.getData();
+        }
+
+        if (currentUri.equals(LocationEntry.CONTENT_URI)) {
+            setTitle(getString(R.string.add_location_title));
+        } else {
+            setTitle(getString(R.string.edit_location_title));
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
     }
