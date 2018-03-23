8cb7f7813ddb7b30e6497e1697e19224f04db9b4
marku <markus.hauschild.negativ@googlemail.com>
2018-03-23 22:43:56 +0100

    Create references to the GUI objects

--- app/src/main/java/com/mahausch/perfectweekend/DetailActivity.java
+++ app/src/main/java/com/mahausch/perfectweekend/DetailActivity.java
@@ -10,6 +10,11 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 import static com.mahausch.perfectweekend.data.LocationContract.BASE_CONTENT_URI;
 import static com.mahausch.perfectweekend.data.LocationContract.LocationEntry.CONTENT_URI;
@@ -21,10 +26,20 @@ public class DetailActivity extends AppCompatActivity implements LoaderManager.L
     private static final int LOCATION_DETAIL_LOADER_ID = 1;
     private long locationID;
 
+    @BindView(R.id.detail_image)
+    ImageView locationImageView;
+
+    @BindView(R.id.location_name)
+    TextView locationNameTextView;
+
+    @BindView(R.id.location_description)
+    TextView locationDescriptionTextView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.location_detail);
+        ButterKnife.bind(this);
 
         locationID = getIntent().getLongExtra(EXTRA_LOCATION_ID, 0);
 
