07d9b5c1d2fc7f8a2d0b19cdb35e398d522d56a7
marku <markus.hauschild.negativ@googlemail.com>
2018-03-23 22:27:27 +0100

    Add CursorLoader and override required methods

--- app/src/main/java/com/mahausch/perfectweekend/DetailActivity.java
+++ app/src/main/java/com/mahausch/perfectweekend/DetailActivity.java
@@ -1,18 +1,43 @@
 package com.mahausch.perfectweekend;
 
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 
-public class DetailActivity extends AppCompatActivity {
+import static com.mahausch.perfectweekend.data.LocationContract.LocationEntry.CONTENT_URI;
+
+public class DetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     public static final String EXTRA_LOCATION_ID = "com.mahausch.perfectweekend.extra.LOCATION_ID";
+    public static final int LOCATION_DETAIL_LOADER_ID = 1;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.location_detail);
+
+        getSupportLoaderManager().initLoader(LOCATION_DETAIL_LOADER_ID, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this, CONTENT_URI, null,
+                null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        data.moveToFirst();
+        adapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
 
     }
 }
