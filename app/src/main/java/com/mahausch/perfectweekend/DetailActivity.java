1f55c080b3dd73023816e14ad4b06eca548cebe5
marku <markus.hauschild.negativ@googlemail.com>
2018-03-24 10:22:34 +0100

    Set back button to toolbar

--- app/src/main/java/com/mahausch/perfectweekend/DetailActivity.java
+++ app/src/main/java/com/mahausch/perfectweekend/DetailActivity.java
@@ -7,9 +7,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.app.NavUtils;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -36,15 +39,33 @@ public class DetailActivity extends AppCompatActivity implements LoaderManager.L
     @BindView(R.id.location_description)
     TextView locationDescriptionTextView;
 
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.location_detail);
         ButterKnife.bind(this);
 
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
         locationID = getIntent().getLongExtra(EXTRA_LOCATION_ID, 0);
 
         getSupportLoaderManager().initLoader(LOCATION_DETAIL_LOADER_ID, null, this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
