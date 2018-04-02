22defae25526a5370e92d3b23a408c8f59fba3d8
marku <markus.hauschild.negativ@googlemail.com>
2018-04-02 13:02:43 +0200

    Add dialog for up button to ask user wether to discard changes or keep editing if changes were made

--- app/src/main/java/com/mahausch/perfectweekend/EditorActivity.java
+++ app/src/main/java/com/mahausch/perfectweekend/EditorActivity.java
@@ -20,12 +20,14 @@ import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NavUtils;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -468,6 +470,28 @@ public class EditorActivity extends AppCompatActivity implements
                     }
                 };
         showUnsavedChangesDialog(discardButtonClickListener);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if (!locationHasChanged) {
+                    NavUtils.navigateUpFromSameTask(EditorActivity.this);
+                    return true;
+                }
+                DialogInterface.OnClickListener discardButtonClickListener =
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                NavUtils.navigateUpFromSameTask(EditorActivity.this);
+                            }
+                        };
+                showUnsavedChangesDialog(discardButtonClickListener);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
