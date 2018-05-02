72294207aef60544b3936157d6a5e606be570c8c
marku <markus.hauschild.negativ@googlemail.com>
2018-05-02 11:40:37 +0200

    Change intentService to JobIntentService due to background service restrictions in Android O

--- app/src/main/java/com/mahausch/perfectweekend/widget/WidgetUpdateService.java
+++ app/src/main/java/com/mahausch/perfectweekend/widget/WidgetUpdateService.java
@@ -1,54 +1,36 @@
 package com.mahausch.perfectweekend.widget;
 
 
-import android.app.IntentService;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
-import android.support.annotation.Nullable;
-import android.support.v4.app.NotificationCompat;
+import android.support.annotation.NonNull;
+import android.support.v4.app.JobIntentService;
 
 import com.mahausch.perfectweekend.R;
 
-public class WidgetUpdateService extends IntentService {
+public class WidgetUpdateService extends JobIntentService {
 
-    public WidgetUpdateService() {
-        super("WidgetUpdateService");
+    /**
+     * Unique job ID for this service.
+     */
+    static final int JOB_ID = 1000;
+
+    /**
+     * Convenience method for enqueuing work in to this service.
+     */
+    public static void enqueueWork(Context context, Intent work) {
+        enqueueWork(context, WidgetUpdateService.class, JOB_ID, work);
     }
 
     @Override
-    protected void onHandleIntent(@Nullable Intent intent) {
-
+    protected void onHandleWork(@NonNull Intent intent) {
         AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
         int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this, LocationWidgetProvider.class));
         //Trigger data update to handle the ListView widgets and force a data refresh
         appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_list);
         //Now update all widgets
         LocationWidgetProvider.updateWidgets(this, appWidgetManager, appWidgetIds);
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-
-        if (Build.VERSION.SDK_INT >= 26) {
-            String CHANNEL_ID = "my_channel_01";
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
-                    "Channel human readable title",
-                    NotificationManager.IMPORTANCE_DEFAULT);
-
-            ((NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE)).createNotificationChannel(channel);
-
-            Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
-                    .setContentTitle("")
-                    .setContentText("").build();
-
-            startForeground(1, notification);
-        }
     }
 }
