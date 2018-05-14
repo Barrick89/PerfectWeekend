1ceb09cfa9ce2954206065c313a46112a5cbd7d3
marku <markus.hauschild.negativ@googlemail.com>
2018-05-14 20:03:26 +0200

    Add test to check if MainActivity displays RecyclerView

--- app/src/androidTest/java/com/mahausch/perfectweekend/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/mahausch/perfectweekend/ExampleInstrumentedTest.java
@@ -2,12 +2,18 @@ package com.mahausch.perfectweekend;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +22,22 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.mahausch.perfectweekend", appContext.getPackageName());
+    }
+
+    //Test if MainActivity is displayed correctly
+    @Test
+    public void checkMainActivityIsVisible_MainActivity() {
+        onView(withId(R.id.recycler_view)).check(matches(isDisplayed()));
     }
 }
